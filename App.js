5102c0b12193aaf14e419f16cdfeeaf69abecc47
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-09-22 21:37:40 -0400

    Initial Commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Component: ComponentsScreen
+  },
+  {
+    initialRouteName: 'Component',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
+  }
+);
+
+export default createAppContainer(navigator);
