d448ad435483594d4c7807b3c3bf3151e37754dd
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-10-01 21:40:58 -0400

    Added Images

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import ImageDetail from '../components/ImageDetail'; 
+import ImageDetail from '../components/ImageDetail';
 
 
 const ImageScreen = () => {
-    return <View>
-        <ImageDetail /> 
-    </View>
-
+    return (
+        <View>
+            <ImageDetail  title="Forest"/>
+            <ImageDetail  title="Beach"/>
+            <ImageDetail  title="Mountain"/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
