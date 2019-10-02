8f74ae18f303f0c89c52ed440df6f8bdb6b21c0b
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-10-01 22:14:30 -0400

    Added image score

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 
-const ImageDetail = props => {
-    return <View> 
-        <Image />
-    <Text>{props.title}</Text>;
-    </View>
+const ImageDetail = ({ imageSource, title, score }) => {
+    return (
+        <View>
+            <Image source={imageSource} />
+            <Text>{title}</Text>
+            <Text>Image Score - {score} </Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
