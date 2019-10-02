4cb8813ed2d41f9a5e84bb24e745d14b8dac6cb9
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-10-01 21:18:13 -0400

    Added Image Detail

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -14,7 +14,12 @@ const HomeScreen = ({ navigation }) => {
       title="Go to List Demo"
       onPress={() => navigation.navigate('List')}
     />
+    <Button 
+      title="Go to Image Demo"
+      onPress={() => navigation.navigate('Image')}
+    />
     </View>
+    
   );
 };
 
---
+++ src/screens/ImageScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import ImageDetail from '../components/ImageDetail'; 
+
+
+const ImageScreen = () => {
+    return <View>
+        <ImageDetail /> 
+    </View>
+
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen; 
