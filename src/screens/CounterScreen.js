2d1d09be8df3917c85a7c40365437788890bd74d
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-10-07 19:18:09 -0400

    Adding Counter Screen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CounterScreen = () => {
+    return <View> 
+        <Text>Current Count:</Text>
+    </View>
+};
