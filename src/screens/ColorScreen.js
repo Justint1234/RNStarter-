405f6eda4d9983837334ec1a3313446da085db82
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-10-09 21:15:53 -0400

    Colors red blue green have been added

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { View, Styles, Button } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 
 const ColorScreen = () => {
     return (
         <View>
             <Button title="Add a Color" />
+            <View style={{ height: 100, width: 100, backgroundColor: 'rgb(0, 255, 0)' }} />
         </View>
     );
 };
+
+const randomRgb = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+};
+
 
 const styles = StyleSheet.create({});
 
