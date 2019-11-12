e5aad42d6faa88dff3199dd1a6f8dc2b2e93d5cd
Justin Williams <justinwilliams@Justins-MacBook-Pro.local>
2019-11-11 22:04:49 -0500

    Added cases for each color and amount

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,6 +5,21 @@ import ColorCounter from "../components/ColorCounter";
 const COLOR_INCREMENT = 15;
 
 const SquareScreen = () => {
+    const reducer = (state, action) => {
+
+        switch (action.colorToChange) {
+            case 'red':
+                return {...state, red: state.red + action.amount};
+            case 'green':
+                    return {...state, green: state.green + action.amount};
+            case 'blue':
+                    return {...state, blue: state.blue + action.amount};
+            default:    
+            return state;         
+        }
+    };
+
+
     const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
   return (
     <View>
