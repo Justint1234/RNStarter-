1995e3c9a5e00496e95a44a1e7a7b3ef44137bea
Justin Williams <justinwilliams@Justins-MBP.lan>
2019-11-26 20:57:44 -0500

    enter name in textbox

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -4,15 +4,18 @@ import { View, Text, StyleSheet, TextInput } from 'react-native';
 const TextScreen = () => {
     const [name, setName] = useState('');
 
-    
+
     return (
     <View>
+        <Text>Enter Name:</Text>
         <TextInput 
         style={styles.input} 
         autoCapitalize="none"
         autoCorrect={false}
-        value="hi there"
+        value={name}
+        onChangeText={newValue => setName(newValue)}
         />
+        <Text>My Name is {name}</Text>
     </View>
     );
 
