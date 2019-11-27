0572d1df235d19b21f3145df340fb16e33406ce0
Justin Williams <justinwilliams@Justins-MBP.lan>
2019-11-26 21:02:30 -0500

    setting password

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,20 +2,21 @@ import React, {useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
 
     return (
     <View>
-        <Text>Enter Name:</Text>
+        <Text>Enter Password:</Text>
         <TextInput 
         style={styles.input} 
         autoCapitalize="none"
         autoCorrect={false}
-        value={name}
+        value={password}
         onChangeText={newValue => setName(newValue)}
         />
         <Text>My Name is {name}</Text>
+        {false ? <Text>That was true</Text> : null}
     </View>
     );
 
